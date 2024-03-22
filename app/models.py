1556e0e0d4594d3ba97efec3019f4d8997ff88aa
Supakorn Charamat <ohmsupakorn@gmail.com>
2024-03-22 11:50:16 +0700

    allow null timestamp

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Store(models.Model):
     store_id = models.IntegerField(unique=True)
     store_location = models.CharField(max_length=100)
-    store_timestamp = models.DateTimeField(default=timezone.now)
+    store_timestamp = models.DateTimeField(default=timezone.now, null=True)
 
     def __str__(self):
         return f"{self.store_id} - {self.store_location} - {self.store_timestamp}"
@@ -12,10 +12,10 @@ class Store(models.Model):
 class Products(models.Model):
     product_id = models.IntegerField(unique=True)
     product_name = models.CharField(max_length=100)
-    product_timestamp = models.DateTimeField(default=timezone.now)
+    product_timestamp = models.DateTimeField(default=timezone.now, null=True)
     def __str__(self):
-        return f"{self.product_id} - {self.product_name} - {self.store_timestamp}"
-    
+        return f"{self.product_id} - {self.product_name} - {self.store_timestamp}"    
+
 # class Sales(models.Model):
 #     transaction_id = models.IntegerField(unique=True)
 #     transaction_date = models.DateField()
