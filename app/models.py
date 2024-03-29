7818ced37792fab03063faa1584f0c9cdd464d38
Supakorn Charamat <ohmsupakorn@gmail.com>
2024-03-29 11:36:18 +0700

    add 0010_sales_product_id_sales_store_id

--- app/models.py
+++ app/models.py
@@ -17,11 +17,14 @@ class Products(models.Model):
     def __str__(self):
         return f"{self.product_id} - {self.product_name} - {self.store_timestamp}"    
 
+
 class Sales(models.Model):
     transaction_id = models.IntegerField()
     transaction_date = models.DateField(default=timezone.now, null=True)
     transaction_time = models.TimeField(default=timezone.now, null=True)
     transaction_qty = models.IntegerField()
+    store_id = models.IntegerField(null=True)
+    product_id = models.IntegerField(null=True)
     unit_price = models.FloatField()
     product_category = models.CharField(max_length=100)
     product_name = models.CharField(max_length=100)
@@ -29,3 +32,4 @@ class Sales(models.Model):
     
     def __str__(self):
         return f"{self.transaction_id} - {self.transaction_qty} - {self.unit_price} - {self.product_category} - {self.product_name} - {self.product_detail}"
+    
