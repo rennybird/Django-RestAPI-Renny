f5c23b044d14cbc8e44f51f4b5218a3d3a6ee820
Supakorn Charamat <ohmsupakorn@gmail.com>
2024-04-11 22:48:01 +0700

    alter sales

--- app/models.py
+++ app/models.py
@@ -22,13 +22,13 @@ class Sales(models.Model):
     transaction_id = models.IntegerField()
     transaction_date = models.DateField(default=timezone.now, null=True)
     transaction_time = models.TimeField(default=timezone.now, null=True)
-    transaction_qty = models.IntegerField()
+    transaction_qty = models.IntegerField(null=True)
     store_id = models.IntegerField(null=True)
     product_id = models.IntegerField(null=True)
     unit_price = models.FloatField()
     product_category = models.CharField(max_length=100)
-    product_name = models.CharField(max_length=100)
-    product_detail = models.TextField()
+    product_name = models.CharField(max_length=100, null=True)
+    product_detail = models.TextField(null=True)
     
     def __str__(self):
         return f"{self.transaction_id} - {self.transaction_qty} - {self.unit_price} - {self.product_category} - {self.product_name} - {self.product_detail}"
